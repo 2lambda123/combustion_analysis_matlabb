15ca2a88fd4f17863a816e9b9ea8bf21832459b1
priybrat-s <30745369+priybrat-s@users.noreply.github.com>
2017-08-08 10:20:37 +0530

    Update and rename heatrelease_att.m to pressuresmooth.m

---
+++ pressuresmooth.m
@@ -0,0 +1,23 @@
+%% this programtakes Pressure matrix formed using "th.m"and averaged smoothed pressure data is returned
+
+p=input('value of p'); % enter "p" where is "p" is the 
+% pressure matrix formed by th.m
+pavg=mean(ptemp); % pressure data averaged 
+psmo=p;
+[m,n]=size(p); 
+i=0; 
+for j=0:10:n-10 
+n1=psmo(j+1:j+10); 
+i=[i,mean(n1)]; 
+end 
+pdata=i(2:end); 
+dp=diff(pdata); 
+[i,j]=size(dp);
+x=1:j; 
+plot(x,dp) 
+%%% Subroutine to ensure thatintake pressure is 1 Atm%%% 
+if pdata(1)<1 
+add=1-pdata(1); 
+pdata=pdata+add; 
+end 
+% %% Subroutine ends%%% 
